a25b9e2183eafa655e7485c4ebeceb96b08ebe0f
Ana Hurtado <anahurtado0104@gmail.com>
2024-07-31 11:49:19 -0500

    Footer finalizado, pendiente validacion

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,32 +4,42 @@ import logo from "../assets/images/logo.jpg";
 function Footer() {
   return (
     <footer>
-      <img src={logo} alt="logo" />
-      <ul>
-        <li>
-          <a href="/home">Home</a>
-        </li>
-        <li>
-          <a href="/about-us">About us</a>
-        </li>
-        <li>
-          <a href="/contact-us">Contact us</a>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <i className="bi bi-instagram"></i>
-        </li>
-        <li>
-          <i className="bi bi-twitter"></i>
-        </li>
-        <li>
-          <i className="bi bi-facebook"></i>
-        </li>
-      </ul>
+      <div className="footer-section-1">
+        <img src={logo} alt="logo" />
+        <ul className="routes">
+          <li>
+            <a href="/home">HOME</a>
+          </li>
+          <li>
+            <a href="/about-us">ABOUT</a>
+          </li>
+          <li>
+            <a href="/contact-us">SERVICES</a>
+          </li>
+          <li>
+            <a href="/contact-us">BLOG</a>
+          </li>
+          <li>
+            <a href="/contact-us">CONTACT</a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <i className="bi bi-instagram"></i>
+          </li>
+          <li>
+            <i className="bi bi-twitter"></i>
+          </li>
+          <li>
+            <i className="bi bi-facebook"></i>
+          </li>
+        </ul>
+      </div>
       <p>
-        Copyright 2024 All Rights Reserved | By{" "}
-        <b>punto.equilibrio.slackline</b>
+        Copyright 2024 All Rights Reserved | Designed by
+        <a href="">
+          <b>punto.equilibrio.slackline</b>
+        </a>
       </p>
     </footer>
   );
