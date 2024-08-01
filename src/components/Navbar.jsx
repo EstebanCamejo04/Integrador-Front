98250af20954c503751f6ceb44a0c2952a3b4908
Bautista Sanguinetti <bautiscsc.123@gmail.com>
2024-07-31 21:12:17 -0300

    Initial commit del Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import logo from "../assets/images/logo2.jpg";
+import "./Navbar.css"
+
+const Navbar = () => {
+    return (
+        <nav className='navBar'>
+            <div className='logoDiv'>
+                <img src={logo} alt="homeLogo" />
+
+                <p>Fly Mountain</p>
+            </div>
+            <div>
+                <button>Iniciar Sesion</button>
+                <button>Crea una Cuenta</button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
