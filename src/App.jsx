2654fd9e924cdc949cf13482f9e96de3449b649c
EstebanCamejo <125935044+EstebanCamejo04@users.noreply.github.com>
2024-08-01 19:45:59 -0300

    Subiendo los detalles del producto (#3)
    * Subiendo los detalles del producto
    
    * arreglando la imagen ✨✨

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Detail from "./components/Detail";
 import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 function App() {
-    return (
-        <>
-            <Navbar />
-            <div>
-                <h1>Hola mundo</h1>
-            </div>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <h1>Hola mundo</h1>
+
+        <Routes>
+          <Route path="/detail/:id" element={<Detail />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
