59a1e0b0ede526c4e920560392294fb229b4024f
MateoMigone <mateomigone@gmail.com>
2024-08-02 18:11:45 -0300

    linkeando productos del home con detalle de producto

--- src/components/common/ProductCard.jsx
+++ src/components/common/ProductCard.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "../../styles/ProductCard.css";
+import { Link } from "react-router-dom";
 
-const ProductCard = ({ img, title }) => {
+const ProductCard = ({ id, img, name }) => {
   return (
     <div className="productCardContainer">
       <img src={img} alt="Product image" />
       <div>
-        <h3>{title}</h3>
-        <button>Ver detalle</button>
+        <h3>{name}</h3>
+        <Link to={`/detail/${id}`}>
+          <button>Ver detalle</button>
+        </Link>
       </div>
     </div>
   );
--- src/components/common/ProductList.jsx
+++ src/components/common/ProductList.jsx
@@ -1,31 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 import "../../styles/ProductList.css";
+import axios from "axios";
 
 const ProductList = () => {
+  const [productList, setProductList] = useState(null);
+
+  useEffect(() => {
+    const fetchProductList = () => {
+      axios
+        .get("/db.json")
+        .then((response) => {
+          setProductList(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching the product list:", error);
+        });
+    };
+
+    fetchProductList();
+  }, [productList]);
+
+  if (!productList) {
+    return <div>Cargando...</div>;
+  }
+
   return (
     <section>
       <h2>Experiencias recomendadas</h2>
       <div className="productList">
-        <ProductCard
-          img={"/images/hamaca.jpg"}
-          title={"Tarde en las hamacas voladoras"}
-        />
-
-        <ProductCard
-          img={"/images/hamaca.jpg"}
-          title={"Tarde en las hamacas voladoras"}
-        />
-
-        <ProductCard
-          img={"/images/hamaca.jpg"}
-          title={"Tarde en las hamacas voladoras"}
-        />
-
-        <ProductCard
-          img={"/images/hamaca.jpg"}
-          title={"Tarde en las hamacas voladoras"}
-        />
+        {productList.map(({ id, name, image }) => {
+          return <ProductCard key={id} id={id} img={image} name={name} />;
+        })}
       </div>
     </section>
   );
