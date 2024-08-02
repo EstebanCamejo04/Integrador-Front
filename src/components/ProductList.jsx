c17801f07cb268faf01f8fe9c92bc001444048a3
MateoMigone <mateomigone@gmail.com>
2024-08-01 22:19:26 -0300

    agregando titulo a la lista de productos

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -5,25 +5,28 @@ import "../styles/ProductList.css";
 const ProductList = () => {
   return (
     <section>
-      <ProductCard
-        img={"/images/hamaca.jpg"}
-        title={"Tarde en las hamacas voladoras"}
-      />
+      <h2>Experiencias recomendadas</h2>
+      <div className="productList">
+        <ProductCard
+          img={"/images/hamaca.jpg"}
+          title={"Tarde en las hamacas voladoras"}
+        />
 
-      <ProductCard
-        img={"/images/hamaca.jpg"}
-        title={"Tarde en las hamacas voladoras"}
-      />
+        <ProductCard
+          img={"/images/hamaca.jpg"}
+          title={"Tarde en las hamacas voladoras"}
+        />
 
-      <ProductCard
-        img={"/images/hamaca.jpg"}
-        title={"Tarde en las hamacas voladoras"}
-      />
+        <ProductCard
+          img={"/images/hamaca.jpg"}
+          title={"Tarde en las hamacas voladoras"}
+        />
 
-      <ProductCard
-        img={"/images/hamaca.jpg"}
-        title={"Tarde en las hamacas voladoras"}
-      />
+        <ProductCard
+          img={"/images/hamaca.jpg"}
+          title={"Tarde en las hamacas voladoras"}
+        />
+      </div>
     </section>
   );
 };
