bf2c75828f61f73a4fe27e023d098fa47ddbf1e6
Daniel Navarro <110798679+dnavarro4@users.noreply.github.com>
2024-08-01 22:03:37 -0300

    1-Genero carpeta Routes->Admin; 2-Creo carpeta y componente Layout (#6)
    Co-authored-by: Bautisc <84480374+Bautisc@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 import Layout from "./Layout/Layout";
+import Admin from "./Routes/Admin";
 import "./styles/App.css";
 import { routes } from "./utils/routes";
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path={routes.home} element={<Layout />}>
           <Route path={routes.home} element={<Home />} />
+          <Route path="/administracion" element={<Admin />} />
           <Route path={`${routes.detail}/:id`} element={<Detail />} />
         </Route>
       </Routes>
