dc549cacd4d4f305c10d752802da398124b16f69
MateoMigone <mateomigone@gmail.com>
2024-08-05 22:19:26 -0300

    resolviendo conflictos al mergear

--- src/components/pages/Detail.jsx
+++ src/components/pages/Detail.jsx
@@ -1,12 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-<<<<<<< HEAD
 import styles from "../../styles/Detail.module.css";
-=======
 import BackButton from "../common/BackButton";
-import "../../styles/Details.css";
->>>>>>> 07c2beae1ee651d662a11f332973f6cf5185b737
 
 const Detail = () => {
   const { id } = useParams();
@@ -33,15 +29,9 @@ const Detail = () => {
   }
 
   return (
-<<<<<<< HEAD
     <div className={styles.details}>
-      <img src={product.image} alt={product.name} className={styles.img} />
-=======
-    <div className="details">
       <BackButton />
-      <img src={product.image} alt={product.name} className="img" />
->>>>>>> 07c2beae1ee651d662a11f332973f6cf5185b737
-
+      <img src={product.image} alt={product.name} className={styles.img} />
       <div className={styles.products}>
         <h2>Plan: {product.name}</h2>
         <p className={styles.paragraph}>{product.description}</p>
