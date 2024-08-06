07c2beae1ee651d662a11f332973f6cf5185b737
EstebanCamejo <125935044+EstebanCamejo04@users.noreply.github.com>
2024-08-05 21:49:35 -0300

    Arreglado los detalles (#10)

---
+++ src/components/common/BackButton.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const BackButton = () => {
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+
+  return (
+    <button onClick={handleBackClick} style={styles.button}>
+      <img src="/images/back_arrow_icon.svg" alt="Volver" style={styles.icon} />
+      Regresar
+    </button>
+  );
+};
+
+const styles = {
+  button: {
+    background: "none",
+    border: "none",
+    cursor: "pointer",
+    padding: "10px",
+  },
+  icon: {
+    width: "24px",
+    height: "24px",
+  },
+};
+
+export default BackButton;
