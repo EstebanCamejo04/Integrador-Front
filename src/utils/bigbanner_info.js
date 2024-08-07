4eb4c880acc5adad49b7b141e7ddac4c97e4da50
Ana Hurtado <anahurtado0104@gmail.com>
2024-08-07 13:49:25 -0500

    actualizacion de la clase a la imagen de tarjeta de productos

--- src/utils/bigbanner_info.js
+++ src/utils/bigbanner_info.js
@@ -3,7 +3,7 @@ export const bigbanner_info = {
     title: "Tranquilidad y experiencia",
     description: "Text",
     highlighted_description: "Text",
-    image_path_1: "/images/actividad2.jpeg",
+    image_path_1: "/images/hamaca-2.jpg",
     image_path_2: "/images/moon-1.jpg",
     image_path_3: "/images/moon-2.jpg",
   },
