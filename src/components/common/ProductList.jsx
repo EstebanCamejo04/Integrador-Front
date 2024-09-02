488ce34394573f7eb6819335556c51b6f5fc6eb6
Ana Hurtado <anahurtado0104@gmail.com>
2024-09-02 15:31:46 -0500

    merge

--- src/components/common/ProductList.jsx
+++ src/components/common/ProductList.jsx
@@ -1,20 +1,61 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 import styles from "../../styles/ProductList.module.css";
 import { useContextGlobal } from "../../context/Context";
 import "react-datepicker/dist/react-datepicker.css";
 import DatePicker from "react-datepicker";
 import searchStyles from "../../styles/SearchProducts.module.css";
+import axios from "axios";
 
 const ProductList = () => {
+  const keyWords = [
+    "playa",
+    "juegos",
+    "cerro",
+    "hamaca",
+    "highline",
+    "Senderismo",
+  ];
   const { state } = useContextGlobal();
   const [search, setSearch] = useState({
     words: "",
+    products: [],
+    startDate: null,
+    endDate: null,
+    suggestionWords: [],
   });
 
-  const [startDate, setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null);
+  useEffect(() => {
+    setSearch({
+      ...search,
+      products: shuffleArray(state.products),
+    });
+  }, [state.products]);
 
+  const updateState = (e) => {
+    setSearch({
+      ...search,
+      suggestionWords: e.target.value
+        ? keyWords.filter((key) => {
+            return key.toLowerCase().includes(e.target.value.toLowerCase());
+          })
+        : [],
+      [e.target.name]: e.target.value,
+    });
+  };
+  const wordSelected = (e) => {
+    setSearch({
+      ...search,
+      words: e.target.textContent,
+      suggestionWords: e.target.textContent
+        ? keyWords.filter((key) => {
+            return key
+              .toLowerCase()
+              .includes(e.target.textContent.toLowerCase());
+          })
+        : [],
+    });
+  };
   const shuffleArray = (array) => {
     let shuffledArray = array.slice();
     for (let i = shuffledArray.length - 1; i > 0; i--) {
@@ -27,13 +68,37 @@ const ProductList = () => {
     return shuffledArray;
   };
   const searchProducts = (e) => {
-    console.log("buscando productos");
-  };
-  const updateState = (e, key) => {
-    setSearch({
-      ...search,
-      key: e.target.value,
-    });
+    e.preventDefault();
+    e.stopPropagation();
+    if ((search.startDate && search.endDate) || search.words) {
+      const url = "http://localhost:3000/api/search-products";
+      axios
+        .get(url, {
+          params: {
+            words: search.words,
+            start: search.startDate
+              ? search.startDate.toISOString().split("T")[0]
+              : null,
+            end: search.endDate
+              ? search.endDate.toISOString().split("T")[0]
+              : null,
+          },
+        })
+        .then((response) => {
+          setSearch({
+            ...search,
+            products: response.data,
+          });
+        })
+        .catch((error) => {
+          console.error("Error searching the product list:", error);
+        });
+    } else {
+      setSearch({
+        ...search,
+        products: shuffleArray(state.products),
+      });
+    }
   };
 
   if (!state.products) {
@@ -41,48 +106,73 @@ const ProductList = () => {
   }
 
   return (
-    <>
+    <div>
       <section className={searchStyles.searchProduct}>
         <h3>Realizar búsqueda</h3>
         <p>
-          Puedes buscar por palabras clave, frases específicas, nombres,
-          categoría o ubicación{" "}
+          Puedes buscar por palabras clave, rango de fechas, descripción,
+          nombre, categoría o ubicación de la experiencia{" "}
         </p>
         <form onSubmit={searchProducts}>
-          <DatePicker
-            selected={startDate}
-            onChange={(dates) => {
-              const [start, end] = dates;
-              setStartDate(start);
-              setEndDate(end);
-            }}
-            startDate={startDate}
-            endDate={endDate}
-            selectsRange
-            placeholderText="Selecciona un rango de fechas"
-            className="form-control"
-          />
-
-          <input
-            type="text"
-            name="words"
-            placeholder="Buscar productos"
-            value={search.words}
-            onChange={updateState}
-          />
-
-          <button type="submit">Realizar búsqueda</button>
+          <div>
+            <DatePicker
+              selected={search.startDate}
+              onChange={(dates) => {
+                const [start, end] = dates;
+                setSearch({
+                  ...search,
+                  startDate: start,
+                  endDate: end,
+                });
+              }}
+              startDate={search.startDate}
+              endDate={search.endDate}
+              selectsRange
+              placeholderText="Selecciona un rango de fechas"
+            />
+          </div>
+          <div className={searchStyles.words}>
+            <input
+              type="text"
+              name="words"
+              placeholder="Buscar productos"
+              value={search.words}
+              onChange={updateState}
+              autoComplete="off"
+            />
+            {search.suggestionWords.length > 0 && (
+              <ul>
+                {search.suggestionWords.map((word, i) => {
+                  return (
+                    <li key={i} onClick={wordSelected}>
+                      {word}
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
+          </div>
+          <button
+            type="submit"
+            className={
+              search.startDate && !search.endDate
+                ? searchStyles.disableButton
+                : ""
+            }
+          >
+            Realizar búsqueda
+          </button>
         </form>
       </section>
       <section className={styles.productListContainer}>
         <h2 className={styles.productListTitle}>Experiencias recomendadas</h2>
         <div className={styles.productList}>
-          {shuffleArray(state.products).map(({ id, name }) => {
-            return <ProductCard key={id} id={id} name={name} />;
+          {search.products.map((item) => {
+            return <ProductCard key={item.id} id={item.id} name={item.name} />;
           })}
         </div>
       </section>
-    </>
+    </div>
   );
 };
 
