70c31e813227a4577f73ea67d1d95ed3e8b09deb
Bautista Sanguinetti <bautiscsc.123@gmail.com>
2024-09-05 15:47:57 -0300

    review del pr y un par de fixes

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -20,8 +20,8 @@ export const initialState = {
       description: "",
     },
   },
-  productsCategory: "",
-};
+  productsCategory: [],
+}
 
 const reducer = (state, action) => {
   switch (action.type) {
