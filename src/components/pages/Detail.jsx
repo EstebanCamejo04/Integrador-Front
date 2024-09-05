748aaed12d6d2fa4051beca99187c4977f0e8491
EstebanCamejo <125935044+EstebanCamejo04@users.noreply.github.com>
2024-09-05 19:04:09 -0300

    Feature/calender (#35)
    * calendario de reservas
    
    * calendario de reservas

--- src/components/pages/Detail.jsx
+++ src/components/pages/Detail.jsx
@@ -1,14 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-import styles from "../../styles/Detail.module.css";
 import BackButton from "../common/BackButton";
 import { useContextGlobal } from "../../context/Context";
 import { featureIcons } from "../../utils/feature_icons";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import styles from "../../styles/Detail.module.css";
+
+// Fechas ocupadas harcodeadas
+const occupiedDates = [
+  new Date("2024-09-05T00:00:00"),
+  new Date("2024-09-10T00:00:00"),
+  new Date("2024-09-15T00:00:00"),
+  new Date("2024-09-20T00:00:00"),
+  new Date("2024-09-18T00:00:00"),
+  new Date("2024-10-18T00:00:00"),
+  new Date("2024-10-10T00:00:00"),
+];
+
+const availableHours = ["10:00", "13:00", "15:00", "17:00"];
 
 const Detail = () => {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedTime, setSelectedTime] = useState(null);
+  const [error, setError] = useState(null);
+  const [showTimePicker, setShowTimePicker] = useState(false);
+  const [fetchDatesError, setFetchDatesError] = useState(false);
   const { state } = useContextGlobal();
   const user = state.user ? state.user : {};
   const { validAdmin, validUser } = state;
@@ -23,6 +43,7 @@ const Detail = () => {
 
         const productData = response.data;
         console.log(response.data);
+
         // Aca le estoy pegando a las features
         const features = productData.product_feature.map((pf) => ({
           icon: featureIcons[pf.feature.name_alias], // Mientras no hay iconos
@@ -32,13 +53,20 @@ const Detail = () => {
         console.log(features);
 
         setProduct({ ...productData, features });
+        //cambiar aca a distinto para simular el error
+        if (occupiedDates.length === 0) {
+          throw new Error("No se pudieron cargar las fechas ocupadas");
+        }
+        handleDateChange(new Date());
       } catch (error) {
         console.error("Error fetching the product:", error);
+        setFetchDatesError(true);
       }
     };
 
     fetchProduct();
   }, [id]);
+
   const handleRentClick = () => {
     if (validUser || validAdmin) {
       alert("Reserva realizada con éxito, gracias por confiar en nosotros!");
@@ -46,6 +74,31 @@ const Detail = () => {
       alert("Debes iniciar sesión o registrarte para alquilar un producto.");
       navigate("/login");
     }
+  };
+
+  const handleDateChange = (date) => {
+    if (
+      date &&
+      occupiedDates.some(
+        (occupiedDate) =>
+          new Date(occupiedDate).toDateString() ===
+          new Date(date).toDateString()
+      )
+    ) {
+      setError("La fecha seleccionada no tiene cupos, consulte otra.");
+      setShowTimePicker(false);
+      return;
+    } else {
+      setError(null);
+      setShowTimePicker(true);
+    }
+
+    setSelectedDate(date);
+    setSelectedTime(null);
+  };
+
+  const handleTimeChange = (event) => {
+    setSelectedTime(event.target.value);
   };
 
   if (!product) {
@@ -68,6 +121,64 @@ const Detail = () => {
           Reservar
         </button>
       </div>
+      <div className={styles.datePickerContainer}>
+        <div className={styles.datePicker}>
+          <h3 className={styles.titleTime}>Seleccionar fecha:</h3>
+          {fetchDatesError ? (
+            <p>
+              Ocurrio un error, consulte la agenda en otro momento por favor,
+              gracias.
+            </p>
+          ) : (
+            <DatePicker
+              selected={selectedDate}
+              onChange={handleDateChange}
+              inline
+              minDate={new Date()}
+              dayClassName={(date) => {
+                const dateStr = new Date(date).toDateString();
+                const isOccupied = occupiedDates.some(
+                  (occupiedDate) =>
+                    new Date(occupiedDate).toDateString() === dateStr
+                );
+                return isOccupied
+                  ? styles["datepicker__day--highlighted"]
+                  : undefined;
+              }}
+            />
+          )}
+        </div>
+        {showTimePicker && (
+          <div className={styles.timePicker}>
+            <h3 className={styles.titleTime}>Seleccionar hora:</h3>
+            <div className={styles.timeButtons}>
+              {availableHours.map((hour, index) => (
+                <label key={index} className={styles.timeButton}>
+                  <input
+                    type="radio"
+                    name="time"
+                    value={hour}
+                    checked={selectedTime === hour}
+                    onChange={handleTimeChange}
+                    className={styles.radioInput}
+                  />
+                  {hour}
+                </label>
+              ))}
+            </div>
+          </div>
+        )}
+        {error && <div className={styles.error}>{error}</div>}
+      </div>
+      {selectedDate && selectedTime && (
+        <div className={styles.selectedDateTime}>
+          <p>
+            Su reserva sera para la fecha: {selectedDate.toLocaleDateString()} a
+            las {selectedTime}
+          </p>
+        </div>
+      )}
+
       <div className={styles.features}>
         <h3 className={styles.title}>Características:</h3>
         <ul className={styles.list}>
