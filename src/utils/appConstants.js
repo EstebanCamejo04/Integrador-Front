54c510d82aa0c2faa2658e188b00af74286187e5
alergio <101852890+alergio@users.noreply.github.com>
2024-09-18 20:10:46 -0300

    Update appConstants.js

--- src/utils/appConstants.js
+++ src/utils/appConstants.js
@@ -1,5 +1,5 @@
-// export const API_BASE_URL = "http://18.118.84.155";
-export const API_BASE_URL = "http://localhost";
+export const API_BASE_URL = "http://18.118.84.155";
+// export const API_BASE_URL = "http://localhost";
 export const HEADER_TOKEN = {
   Authorization: `Bearer ${localStorage
     .getItem("token")
